b03893703f23d1ee1f1799a51256a9bb368158fe
vmurali100 <vmurali100@users.noreply.github.com>
2021-03-17 15:52:27 +0530

    Added

--- server/routes/students.js
+++ server/routes/students.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-    res.send('respond with a resource From Students');
+    var connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'password'
+    });
+
+    connection.connect(function (err) {
+        if (err) {
+            console.error('error connecting: ' + err.stack);
+            return;
+        }
+
+        connection.query(`SELECT * FROM school_project.students`, (err, data) => {
+            res.send({ studets: data });
+        })
+    });
+
 });
 
 module.exports = router;
